2692a9af0530dad7c9bf104d67a1be22b06ed4ba
JN Bourrat <jn.brrt@yahoo.fr>
2018-10-31 03:57:00 +0100

    Changed input radios to button

--- src/app/TriviaClass.ts
+++ src/app/TriviaClass.ts
@@ -3,31 +3,22 @@ export class TriviaClass {
   type: string;
   difficulty: string;
   question: string;
-  correct_answer: string;
-  incorrect_answers0: string;
-  incorrect_answers1: string;
-  incorrect_answers2: string;
+  answers: string[];
 
-  // Mettre les answers dans un tableau ?
+  // Mettre les answers dans un tableau ? (!)
 
     constructor (
       category: string,
       type: string,
       difficulty: string,
       question: string,
-      correct_answer: string,
-      incorrect_answers0: string,
-      incorrect_answers1: string,
-      incorrect_answers2: string,
+      answers: string[]
       ) {
       this.category = category;
       this.type = type;
       this.difficulty = difficulty;
       this.question = question;
-      this.correct_answer = correct_answer;
-      this.incorrect_answers0 = incorrect_answers0;
-      this.incorrect_answers1 = incorrect_answers1;
-      this.incorrect_answers2 = incorrect_answers2;
+      this.answers = answers;
     }
 }
 
